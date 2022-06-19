0e5c73095d197c1c69c4143ffd92197bc04eac2c
Sachin Gupta <sachin010886@gmail.com>
2022-06-19 07:47:42 +0530

    data management module & function

--- init_setup.sh
+++ init_setup.sh
@@ -3,6 +3,7 @@ echo [$(date)]: "creating environment"
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate environment"
 source activate ./env
+echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "END"
 
